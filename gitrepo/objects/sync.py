d3e36bdb0cf08a74224d04c3edd3e2a8935f5016
Sergiy Kulanov <sergey@kulanov.org.ua>
2018-10-16 15:49:47 +0300

    [workaround] no new changes is not error

--- gitrepo/objects/sync.py
+++ gitrepo/objects/sync.py
@@ -68,6 +68,9 @@ class RepoSync(RepoBase):
         if push_infos:
             for push_info in push_infos:
                 if push_info.flags & push_info.ERROR:
+                    if push_info.summary == '[remote rejected] (no new changes)\n':
+                        logging.info("SKIPPING: '{0}'".format(push_info.summary))
+                        return True, 'No changes'
                     err_msg = ("Push failed for project '{0}': "
                                "{1}".format(self.repo_name, push_info.summary))
                     logging.error(err_msg)
